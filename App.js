a049f92883d12b3eed6dd3dce00b684aaeaf81e4
156082 <gdx156082@student.gdansk.merito.pl>
2025-01-09 15:21:03 +0100

    Update App.js

--- App.js
+++ App.js
@@ -7,12 +7,12 @@ import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc } from "fi
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEP4W_wna31xQHWys-WgDHcvkq4BPqcZM",
-  authDomain: "myapp-6bf4d.firebaseapp.com",
-  projectId: "myapp-6bf4d",
-  storageBucket: "myapp-6bf4d.firebasestorage.app",
-  messagingSenderId: "735119390219",
-  appId: "1:735119390219:web:5a190c019bb904db28b90e",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
